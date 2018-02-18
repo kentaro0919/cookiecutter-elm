bc8d6582800bd221d0ddef5ae229c7d0aaed4ae5
Kentaro Hori <kentaro0919@gmail.com>
2018-02-18 16:40:28 +0900

    move to Webpack

---
+++ {{cookiecutter.app_name}}/src/Main.elm
@@ -0,0 +1,27 @@
+module Main exposing (..)
+
+import Html exposing (program)
+import Msgs exposing (Msg)
+import Models exposing (Model)
+import Update exposing (update)
+import View exposing (view)
+import Subscriptions exposing (subscriptions)
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
