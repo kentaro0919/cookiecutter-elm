6f952e0056013e9c46c0430959c959288527a3bb
Kentaro0919 <kentaro0919@gmail.com>
2018-02-22 12:03:45 +0900

    Update View.elm

--- {{cookiecutter.app_name}}/src/View.elm
+++ {{cookiecutter.app_name}}/src/View.elm
@@ -7,5 +7,6 @@ import Models exposing (Model)
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ text model ]
+    div [class "min-h-screen flex items-center justify-center"]
+        [ h1 [class "text-5xl text-purple font-sans"] [ text model]
+        ]
