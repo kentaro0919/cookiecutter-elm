95b1b82a33a6d1d743a0ac255c5113413aa72229
Kentaro0919 <kentaro0919@gmail.com>
2018-02-27 13:58:52 +0900

    Update webpack.config.js

--- {{cookiecutter.app_name}}/webpack.config.js
+++ {{cookiecutter.app_name}}/webpack.config.js
@@ -11,31 +11,29 @@ module.exports = {
     },
 
     module: {
-        rules: [
-                  {
-        test: /\.(css|scss)$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      }, {
-                test: /\.html$/,
-                exclude: /node_modules/,
-                loader: "file-loader?name=[name].[ext]"
-            }, {
-                test: /\.elm$/,
-                exclude: [
-                    /elm-stuff/, /node_modules/
-                ],
-                loader: "elm-webpack-loader?verbose=true&warn=true"
-            }, {
-                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "url-loader?limit=10000&mimetype=application/font-woff"
-            }, {
-                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                loader: "file-loader"
-            }
-        ],
+        rules: [{
+            test: /\.(css|scss)$/,
+            use: [
+                'style-loader',
+                'css-loader',
+            ]
+        }, {
+            test: /\.html$/,
+            exclude: /node_modules/,
+            loader: "file-loader?name=[name].[ext]"
+        }, {
+            test: /\.elm$/,
+            exclude: [
+                /elm-stuff/, /node_modules/
+            ],
+            loader: "elm-webpack-loader?verbose=true&warn=true"
+        }, {
+            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "url-loader?limit=10000&mimetype=application/font-woff"
+        }, {
+            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+            loader: "file-loader"
+        }],
 
         noParse: /\.elm$/
     },
